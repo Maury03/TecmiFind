bf666a7cd6adbddd5f26e55eaa032492298166de
Maury03 <diegomqh@gmail.com>
2025-04-02 22:19:19 -0600

    Correcciones assets

--- src/components/Marcadores.jsx
+++ src/components/Marcadores.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
+import L from 'leaflet'
 import dataFile from '../assets/campus_data.json';
 
 function Marcadores({ setCampusSeleccionado }) {
+    const defaultIcon = new L.Icon({
+        iconUrl: './markers/marker-icon.png',
+        iconSize: [25, 41],
+        iconAnchor: [12,41],
+        popupAnchor: [1, -34],
+        tooltipAnchor: [16, -28],
+        shadowUrl: './markers/marker-shadow.png',
+        shadowSize: [41, 41],
+        shadowAnchor: [12, 41]
+    })
     try {
         return dataFile.map((campus) => (
             <Marker
